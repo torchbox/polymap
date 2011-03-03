e9cedf99141a6b1ec0a8da64dd8573810628b770
Matt Westcott <matt@west.co.tt>
2011-03-03 17:04:26 +0000

    implement map legend

--- polymap-client/polymap.js
+++ polymap-client/polymap.js
@@ -14,6 +14,39 @@
 				mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 			var map = new google.maps.Map(container.get(0), myOptions);
+			
+			var legend = $('<ul class="legend"></ul>');
+			legend.css({
+				'font-size': '10pt',
+				'border': '1px solid #678AC7',
+				'background-color': 'white',
+				'list-style-type': 'none',
+				'padding': '4px 8px 4px 8px',
+				'margin-right': '6px',
+				'line-height': '1.3em',
+				'-moz-box-shadow': '2px 2px 3px rgba(0, 0, 0, 0.347656)',
+				'-webkit-box-shadow': '2px 2px 3px rgba(0, 0, 0, 0.347656)',
+				'box-shadow': '2px 2px 3px rgba(0, 0, 0, 0.347656)'
+			});
+			var showLegend = false;
+			for (var i = 0; i < description.styles.length; i++) {
+				var style = description.styles[i];
+				if (style.label) {
+					showLegend = true;
+					var legendItem = $('<li></li>').text(style.label);
+					var swatch = $('<span></span>').css({
+						'padding': '0 8px 0 8px',
+						'margin-right': '8px',
+						'background-color': style.fillColour
+					})
+					legendItem.prepend(swatch);
+					legend.append(legendItem);
+				}
+			}
+			if (showLegend) {
+				map.controls[google.maps.ControlPosition.RIGHT_TOP].push(legend.get(0));
+			}
+			
 			var kml = new google.maps.KmlLayer(kmlUrl, {preserveViewport: description.preserveViewport});
 			kml.setMap(map);
 		}
