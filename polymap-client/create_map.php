99f299a3c22c06521f424292098021821e1266e2
Matt Westcott <matt@west.co.tt>
2011-03-03 12:12:58 +0000

    Use live site and hook up an actual map :-D

--- polymap-client/create_map.php
+++ polymap-client/create_map.php
@@ -1,7 +1,7 @@
 <?php
 	$input = file_get_contents('php://input');
 	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL,'http://localhost:8081/create');
+	curl_setopt($ch,CURLOPT_URL,'http://tbxpolymap.appspot.com/create');
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER, TRUE);
 	curl_setopt($ch,CURLOPT_POST, TRUE);
 	curl_setopt($ch,CURLOPT_POSTFIELDS, $input);
